1a727f7b3286bd215a8bca4baf99800fadaac75e
Benoit <benoit.guillot@akretion.com>
2021-05-03 14:01:42 +0200

    remove scope

--- auth_api_key/models/auth_api_key.py
+++ auth_api_key/models/auth_api_key.py
@@ -64,8 +64,6 @@ class AuthApiKey(models.Model):
         if not self.env.user.has_group("base.group_system"):
             raise AccessError(_("User is not allowed"))
         domain = []
-        if scope:
-            domain = [("scope", "=", scope)]
         for api_key in self.search(domain):
             if api_key.key and consteq(key, api_key.key):
                 return api_key.id
