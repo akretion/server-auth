b636fa861a3d84a54e180c78214456730b9ad9f3
Benoit <benoit.guillot@akretion.com>
2022-04-14 16:53:00 +0200

    14.0 Add method to generate api key and add report

--- auth_api_key/__manifest__.py
+++ auth_api_key/__manifest__.py
@@ -10,5 +10,10 @@
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-auth",
     "development_status": "Production/Stable",
-    "data": ["security/ir.model.access.csv", "views/auth_api_key.xml"],
+    "data": [
+        "security/ir.model.access.csv",
+        "report/report_key_barcode.xml",
+        "views/auth_api_key.xml",
+        "views/report_api_key.xml",
+    ],
 }
